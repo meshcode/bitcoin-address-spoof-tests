e646c2705e1d323b614ad59ae38bef4769bad335
Mateusz <sandbox@meshcode.ca>
2020-06-12 00:37:39 -0400

    Revised commentary and formatting.

--- src/main/java/ca/meshcode/HTTPServer.java
+++ src/main/java/ca/meshcode/HTTPServer.java
@@ -1,42 +1,70 @@
+//same package as main function
 package ca.meshcode;
 
-//Example 5.13 HTTP Server
+
+//imports the networking libraries
 import java.io.*;
 import java.net.*;
 
+//
+//Comments, formatting and revision with bitcoin address idea by MK
+//Based on an example from an excellent source by master Perry Xiao
+//
+//Main Class
 public class HTTPServer {
+    //the constructor
     protected void start() {
+        //create a new socket
         ServerSocket s;
-
-        System.out.println("Starting up HTTP Server...");
+        //turn on the server
+        System.out.println("Flipped the ON switch for Gangsta HTTP server.");
+        //start the process
         try {
+            //create the new socket on designated port
             s = new ServerSocket(8088);
+            //catch any issues
         } catch (Exception e) {
-            System.out.println("Error: " + e);
+            //report error if not possible to create the socket
+            System.out.println("Dang Error when creating socket: " + e);
+            //step out of the try
             return;
         }
 
-        System.out.println("Waiting... ");
+        System.out.println("... ");
+        //[!! MAKE BETTER] play an infinite loop to listen to incoming connection
         for (;;) {
+            //try the
             try {
+                //accept a connection as "remote" when incoming client
                 Socket remote = s.accept();
-                System.out.println("Connected....");
-                BufferedReader in = new BufferedReader(new InputStreamReader(
-                        remote.getInputStream()));
+                //report connection to console
+                System.out.println("ZAP. Connected.");
+                //
+                //MAIN I/O CONNECTION PART
+                //Create input stream "in"
+                BufferedReader in = new BufferedReader(new InputStreamReader(remote.getInputStream()));
+                //Create output stream "out"
                 PrintWriter out = new PrintWriter(remote.getOutputStream());
-
+                //
+                //create a dot string
                 String str = ".";
+                //[!! MAKE BETTER] - loop while this string is not empty space
                 while (!str.equals("")){
+                    //read a line from the incoming stream
                     str = in.readLine();
-                    System.out.println(str);
+                    //report this line to the console
+                    System.out.println("Incoming data line from client: " + str);
                 }
+                //output to the client:
                 out.println("HTTP/1.1 200 OK");
                 out.println("Content-Type: text/html");
                 out.println("Server: Java HTTP Server");
+                //the client will probably stop reading once it gets this empty nullish line
                 out.println("");
+                //
                 // Send the HTML page
-               // out.println("<H1>Hello World!</H1>");
-
+                //out.println("<H1>Hello World!</H1>");
+                //
                 out.println("<!DOCTYPE html>   \n" +
                         "<html>   \n" +
                         "<head>  \n" +
@@ -56,11 +84,15 @@ public class HTTPServer {
                         "    <div class='BitcoinAddressDIV'><h1>13uMuecDkSEps8kGhh1BpVdAHafsEQnqeY</h1></div>  \n" +
                         "</body>     \n" +
                         "</html>  ");
-
+                        //
+                        //send it all to the out door
                         out.flush();
+                //close the connection once it received all this
+                //in this way possibly because it's one thread it can then take more after it closes (?)
                 remote.close();
+
             } catch (Exception e) {
-                System.out.println("Error: " + e);
+                System.out.println("DANG Error, could not do the I/O thing: " + e);
             }
         }
     }
--- src/main/java/ca/meshcode/Main.java
+++ src/main/java/ca/meshcode/Main.java
@@ -1,10 +1,21 @@
-package ca.meshcode;
+/*
+PROGRAM: A Java HTTP Server with HTML CSS content which contains a Bitcoin Address
+AIM: To test the integrity of the Bitocin address using specialized automated processes
+WHY: Because it is easy to perform an MITM attack these days and spoof addresses.
+Good to have the knowledge to perform automated verification on one's own web service.
 
+AUTHOR: Mateusz Kujawski
+DATE: Early June 2020
+ */
+//package
+package ca.meshcode;
+//main class
 public class Main {
+    //main function
     public static void main(String[] args) {
-        //create an HTTP Server with unique name
+        //create an HTTP Server object with unique name
         //this is a call to the HTTPServer class in this package
-        //Assign the class "HTTPServer" to a variable "freshHTTPServer" and run its constructor HTTPServer()
+        //Assign the class "HTTPServer" to an instance/variable "freshHTTPServer" and run its constructor HTTPServer()
         HTTPServer freshHTTPServer = new HTTPServer();
         //
         //start the server (a built-in method)
